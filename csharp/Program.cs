60986b56330338b840a917dcc577d4e4d52f990f
huangyingw@fareast <huangyingw@gmail.com>
2010-04-20 15:22:37 +0800

    n

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -54,11 +54,31 @@ namespace csharp
     abstract class D:B
     {
     }
+    public class Person
+    {
+        public int age;
+        public string name;
+
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person(50, "Fred"));
+            people.Add(new Person(30, "John"));
+            people.Add(new Person(26, "Andrew"));
+            people.Add(new Person(24, "Xavier"));
+            people.Add(new Person(5, "Mark"));
+            people.Add(new Person(6, "Cameron"));
+
 
         }
     }
