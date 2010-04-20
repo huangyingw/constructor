9d4f8e33f5122412fd6e3281eab47d14a90c25aa
huangyingw@fareast <huangyingw@gmail.com>
2010-04-20 15:23:49 +0800

    n

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -78,6 +78,20 @@ namespace csharp
             people.Add(new Person(24, "Xavier"));
             people.Add(new Person(5, "Mark"));
             people.Add(new Person(6, "Cameron"));
+            Console.WriteLine("Unsorted list");
+            people.ForEach(delegate(Person p) { Console.WriteLine(String.Format("{0} {1}", p.age, p.name)); });
+
+            List<Person> young = people.FindAll(delegate(Person p) { return p.age < 25; });
+            Console.WriteLine("Age is less than 25");
+            young.ForEach(delegate(Person p) { Console.WriteLine(String.Format("{0} {1}", p.age, p.name)); });
+
+            Console.WriteLine("Sorted list, by name");
+            people.Sort(delegate(Person p1, Person p2) { return p1.name.CompareTo(p2.name); });
+            people.ForEach(delegate(Person p) { Console.WriteLine(String.Format("{0} {1}", p.age, p.name)); });
+
+            people.Sort(delegate(Person p1, Person p2) { return p1.age.CompareTo(p2.age); });
+            Console.WriteLine("Sorted list, by age");
+            people.ForEach(delegate(Person p) { Console.WriteLine(String.Format("{0} {1}", p.age, p.name)); });
 
 
         }
